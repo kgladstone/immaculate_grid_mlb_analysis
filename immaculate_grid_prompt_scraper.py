b8ee5d24b432e829a5d03eae6453f49d81cf67fb
Keith Gladstone <gladstoneka@gmail.com>
2024-03-07 19:22:36 -0500

    Create immaculate_grid_prompt_scraper.py
    basic script to get all the immaculate grid prompts into an easy CSV and saves it locally;
    
    hard codes the final date of pull

---
+++ immaculate_grid_prompt_scraper.py
@@ -0,0 +1,33 @@
+# Immaculate Grid Prompt Extractor
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+MAX_GRID_ID = 340
+
+def get_grid_as_list(grid_id):
+    print("Attempting to pull Immaculate Grid #{}".format(str(grid_id)))
+    url = "https://www.immaculategrid.com/grid-" + str(grid_id)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    buttons_with_aria_label = soup.find_all("button", attrs={'aria-label': True})
+    list_of_labels = list()
+    list_of_labels.append(str(grid_id))
+    for button in buttons_with_aria_label:
+        list_of_labels.append(button['aria-label'])
+    return list_of_labels
+
+def write_lists_of_lists_csv(list_of_lists, output_fn):
+    with open(output_fn, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(list_of_lists)
+    print(f'Data written to {output_fn}')
+    return
+
+def main():
+    grid_data = list()
+    for i in range(1, MAX_GRID_ID):
+        grid_data.append(get_grid_as_list(i))
+    write_lists_of_lists_csv(data, "immaculate_grid_prompt_data.csv")
+    
+main()
