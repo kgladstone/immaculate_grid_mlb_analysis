a7c7cdf58d21e05da1ef93532e708d08fc2624b5
kgladstone <gladstoneka@gmail.com>
2024-10-19 15:40:41 -0400

    refactor prompt scraper

---
+++ .ipynb_checkpoints/immaculate_grid_prompt_scraper-checkpoint.py
@@ -0,0 +1,94 @@
+# Immaculate Grid Prompt Extractor
+import requests
+from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+import pandas as pd
+import ast
+
+GRID_STORAGE_PATH = './prompts.csv'
+IMM_GRID_START_DATE = datetime(2023, 4, 2)
+
+def get_today_grid_id():
+    """
+    Calculate the Immaculate Grid ID for today.
+    """
+    today = datetime.now()
+    return (today - IMM_GRID_START_DATE).days
+
+def _parse_raw_to_tuple(raw):
+    """
+    Parse a string in the format 'part1 + part2 + part3' into a tuple ('part1', 'part2 + part3').
+    """
+    parts = raw.split(" + ", 1)
+    return tuple(part.strip() for part in parts) if len(parts) > 1 else (parts[0].strip(), '')
+
+def _tokenize_tuples_list(tuples_list):
+    """
+    Flatten the list of tuples into a set of unique tokens
+    """
+    return set(token for tup in tuples_list for token in tup)
+
+def get_tokenized_df(df):
+    tokenized_df = pd.DataFrame(columns=['grid_id', 'token'])
+    for i, row in df.iterrows():
+        token_dict = dict()
+        token_dict['grid_id'] = i
+        token_dict['token'] = list(_tokenize_tuples_list(row[1:]))
+        tokenized_df_i = pd.DataFrame(token_dict)
+        tokenized_df = pd.concat([tokenized_df, tokenized_df_i], ignore_index=True, axis=0)
+    return tokenized_df
+
+def fetch_grid_online(grid_id):
+    """
+    Fetch the Immaculate Grid data from the web by grid ID.
+    """
+    print("Attempting to pull Immaculate Grid #{}".format(str(grid_id)))
+    url = f"https://www.immaculategrid.com/grid-{grid_id}"
+    response = requests.get(url)
+    response.raise_for_status()  # Raises HTTPError for bad responses
+    soup = BeautifulSoup(response.text, 'html.parser')
+    buttons = soup.find_all("button", attrs={'aria-label': True})
+    labels = [str(grid_id)]
+    labels.extend(_parse_raw_to_tuple(button['aria-label']) for button in buttons)
+    return labels
+
+def fetch_grids_online(index_list):
+    """
+    Fetch multiple Immaculate Grids and return as a DataFrame.
+    """
+    header = ['grid_id'] + [f"cell{i}" for i in range(1, 10)]
+    grids_data = [fetch_grid_online(i) for i in index_list]
+    return pd.DataFrame(grids_data, columns=header)
+
+def get_grids_as_df_csv(filename):
+    df = pd.read_csv(filename)
+    df_transformed = df.applymap(lambda cell: ast.literal_eval(cell) if isinstance(cell, str) else cell)
+    return df_transformed
+
+if __name__ == "__main__":
+
+    # 1 - Read cached file and determine set of ids
+    print("Reading cached prompts file...")
+    data_previous = pd.read_csv(GRID_STORAGE_PATH)
+    indices_previous = set(data_previous['grid_id'])
+
+    # 2 - Determine set of ids between universe start and today
+    today_index = get_today_grid_id()
+    indices_universe = set(range(1, today_index + 1))
+    
+    # 3 - Determine remaining available ids to pull
+    indices_remaining = list(indices_universe - indices_previous)
+
+    if len(indices_remaining) == 0:
+        print("No new data to pull!")
+    
+    # 4 - Execute the fetch of those ids
+    data_incremental = fetch_grids_online(indices_remaining)
+    
+    # 5 - Merge into resultant dataframe
+    data_combined = pd.concat([data_previous, data_incremental], ignore_index=True)
+
+    # 6 - Save resultant dataframe into original location   
+    print("Saving prompts file...")
+    data_combined.to_csv(GRID_STORAGE_PATH, index=False)
--- refresh_prompts.py
+++ refresh_prompts.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import pandas as pd
 import ast
 
+GRID_STORAGE_PATH = './prompts.csv'
 IMM_GRID_START_DATE = datetime(2023, 4, 2)
 
 def get_today_grid_id():
@@ -15,7 +16,7 @@ def get_today_grid_id():
     today = datetime.now()
     return (today - IMM_GRID_START_DATE).days
 
-def parse_raw_to_tuple(raw):
+def _parse_raw_prompts_to_tuple(raw):
     """
     Parse a string in the format 'part1 + part2 + part3' into a tuple ('part1', 'part2 + part3').
     """
@@ -29,16 +30,16 @@ def _tokenize_tuples_list(tuples_list):
     return set(token for tup in tuples_list for token in tup)
 
 def get_tokenized_df(df):
-    tokenized_df = pd.DataFrame(columns=['index', 'token'])
+    tokenized_df = pd.DataFrame(columns=['grid_id', 'token'])
     for i, row in df.iterrows():
         token_dict = dict()
-        token_dict['index'] = i
+        token_dict['grid_id'] = i
         token_dict['token'] = list(_tokenize_tuples_list(row[1:]))
         tokenized_df_i = pd.DataFrame(token_dict)
         tokenized_df = pd.concat([tokenized_df, tokenized_df_i], ignore_index=True, axis=0)
     return tokenized_df
 
-def get_grid_as_list_online(grid_id):
+def fetch_grid_online(grid_id):
     """
     Fetch the Immaculate Grid data from the web by grid ID.
     """
@@ -49,47 +50,43 @@ def get_grid_as_list_online(grid_id):
     soup = BeautifulSoup(response.text, 'html.parser')
     buttons = soup.find_all("button", attrs={'aria-label': True})
     labels = [str(grid_id)]
-    labels.extend(parse_raw_to_tuple(button['aria-label']) for button in buttons)
+    labels.extend(_parse_raw_prompts_to_tuple(button['aria-label']) for button in buttons)
     return labels
 
-def get_grids_as_df_online(min_index, max_index):
+def fetch_grids_online(index_list):
     """
     Fetch multiple Immaculate Grids and return as a DataFrame.
     """
-    header = ["index"] + [f"cell{i}" for i in range(1, 10)]
-    grids_data = [get_grid_as_list_online(i) for i in range(min_index, max_index + 1)]
+    header = ['grid_id'] + [f"cell{i}" for i in range(1, 10)]
+    grids_data = [fetch_grid_online(i) for i in index_list]
     return pd.DataFrame(grids_data, columns=header)
 
-def get_grids_as_df_csv(filename):
-    df = pd.read_csv(filename)
-    df_transformed = df.applymap(lambda cell: ast.literal_eval(cell) if isinstance(cell, str) else cell)
-    return df_transformed
+def refresh_prompts(GRID_STORAGE_PATH):
+    # 1 - Read cached file and determine set of ids
+    print("Reading cached prompts file...")
+    data_previous = pd.read_csv(GRID_STORAGE_PATH)
+    indices_previous = set(data_previous['grid_id'])
 
-def create_freq_df(df):
-    NUM_DAYS = df['index'].nunique()
-    frequency_dict = {}
-    for index, row in df.iterrows():
-        token = row['token']
-        if token in frequency_dict:
-            frequency_dict[token] += 1
-        else:
-            frequency_dict[token] = 1
-        
-    df = pd.DataFrame(list(frequency_dict.items()), columns=['token', 'freq'])
-    df['freq_pct'] = df['freq'].apply(lambda x: x / NUM_DAYS)
+    # 2 - Determine set of ids between universe start and today
+    today_index = get_today_grid_id()
+    indices_universe = set(range(1, today_index + 1))
+    
+    # 3 - Determine remaining available ids to pull
+    indices_remaining = list(indices_universe - indices_previous)
 
-    df_sorted = df.sort_values(by='freq', ascending=False)        
+    if len(indices_remaining) == 0:
+        print("No new data to pull!")
+    
+    # 4 - Execute the fetch of those ids
+    data_incremental = fetch_grids_online(indices_remaining)
+    
+    # 5 - Merge into resultant dataframe
+    data_combined = pd.concat([data_previous, data_incremental], ignore_index=True)
 
-    return df_sorted
+    # 6 - Save resultant dataframe into original location   
+    print("Saving prompts file...")
+    data_combined.to_csv(GRID_STORAGE_PATH, index=False)
 
-def main():
-    today_grid_id = get_today_grid_id()
-    grids_df = get_grids_as_df_online(1, today_grid_id)
-    grids_df.to_csv('immaculate_grid_prompt_data.csv', index=False)
-    # df = pd.read_csv('immaculate_grid_prompt_data.csv')
-    # df = df.applymap(lambda cell: ast.literal_eval(cell) if isinstance(cell, str) else cell)
-    # tokenized_df = get_tokenized_df(df)
-    # freq_df = create_freq_df(tokenized_df)
-    # freq_df.to_csv('immaculate_grid_token_freq.csv', index=False)
-     
-main()
+
+if __name__ == "__main__":
+    refresh_prompts(GRID_STORAGE_PATH)
